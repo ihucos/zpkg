0c6cdcb51cdcb71a9b0b3bb2718d66406f060609
Irae Hueck Costa <mail@irae.me>
2019-03-29 14:13:50 +0100

    -

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ HOME=~
 INSTALL_TO="$HOME/.local"
 tmp=$(mktemp -d)
 mkdir -p "$tmp/bin" "$tmp/lib/zpkg"
-curl -Lf https://github.com/ihucos/plash/archive/master.tar.gz | tar -xzC "$tmp/lib/zpkg" plash-master/opt/plash --strip-components=2
-curl -Lf https://github.com/ihucos/bchroot/releases/download/0.3/bchroot > $tmp/lib/zpkg/bchroot
-curl -Lf https://raw.githubusercontent.com/ihucos/zpkg/master/zpkg >       $tmp/lib/zpkg/zpkg
+curl -Lf https://github.com/ihucos/plash/archive/master.tar.gz | tar -xzC          $tmp/lib/zpkg plash-master/opt/plash --strip-components=2
+curl -Lf https://github.com/ihucos/bchroot/releases/download/0.3/bchroot >         $tmp/lib/zpkg/bchroot
+curl -Lf https://raw.githubusercontent.com/ihucos/zpkg/master/zpkg >               $tmp/lib/zpkg/zpkg
+curl -Lf https://raw.githubusercontent.com/ihucos/zpkg/master/bootstrap.sh >       $tmp/lib/zpkg/bootstrap.sh
 chmod +x $tmp/lib/zpkg/bchroot
 chmod +x $tmp/lib/zpkg/zpkg
+chmod +x $tmp/lib/zpkg/bootstrap.sh
 ln -s  "$INSTALL_TO/lib/zpkg/zpkg/zpkg" $tmp/bin/zpkg
 echo $tmp
 
