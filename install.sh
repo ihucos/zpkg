ef3bd32e4ce0489a1fe253af495c8608dc49ef4a
Irae Hueck Costa <mail@irae.me>
2019-03-30 00:29:30 +0100

    -

--- install.sh
+++ install.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
 HOME=~
 INSTALL_TO="$HOME/.local"
 tmp=$(mktemp -d)
 mkdir -p "$tmp/bin" "$tmp/lib/zpkg"
-curl -Lf https://github.com/ihucos/plash/archive/master.tar.gz | tar -xzC          $tmp/lib/zpkg plash-master/opt/plash --strip-components=2
-curl -Lf https://github.com/ihucos/bchroot/releases/download/0.3/bchroot >         $tmp/lib/zpkg/bchroot
-curl -Lf https://raw.githubusercontent.com/ihucos/zpkg/master/zpkg >               $tmp/lib/zpkg/zpkg
-curl -Lf https://raw.githubusercontent.com/ihucos/zpkg/master/bootstrap.sh >       $tmp/lib/zpkg/bootstrap.sh
+curl -Lf'#' https://github.com/ihucos/plash/archive/master.tar.gz | tar -xzC          $tmp/lib/zpkg plash-master/opt/plash --strip-components=2
+curl -Lf'#' https://github.com/ihucos/bchroot/releases/download/0.3/bchroot >         $tmp/lib/zpkg/bchroot
+curl -Lf'#' https://raw.githubusercontent.com/ihucos/zpkg/master/zpkg >               $tmp/lib/zpkg/zpkg
+curl -Lf'#' https://raw.githubusercontent.com/ihucos/zpkg/master/bootstrap.sh >       $tmp/lib/zpkg/bootstrap.sh
 chmod +x $tmp/lib/zpkg/bchroot
 chmod +x $tmp/lib/zpkg/zpkg
 chmod +x $tmp/lib/zpkg/bootstrap.sh
 ln -s  "$INSTALL_TO/lib/zpkg/zpkg/zpkg" $tmp/bin/zpkg
-echo $tmp
 
 mkdir -p $INSTALL_TO/lib/zpkg/zpkg $INSTALL_TO/bin
 
 rm -rf $INSTALL_TO/lib/zpkg/zpkg
 mv $tmp/lib/zpkg $INSTALL_TO/lib/zpkg/zpkg
 mv $tmp/bin/zpkg $INSTALL_TO/bin/zpkg
+
+echo "Installed zpkg"
+
+if ! $(command -v python3 > /dev/null); then
+  echo "Please install python3"
+fi
+
+if ! $(command -v unionfs > /dev/null); then
+  echo "Please install unionfs-fuse"
+fi
